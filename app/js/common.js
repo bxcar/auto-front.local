6d36c2fafc486eb164f4bee4c0a09077c5db33e8
bxcar <malanchukdima@mail.ru>
2018-06-14 13:34:18 +0300

    burger done

--- app/js/common.js
+++ app/js/common.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("#owl-slider").owlCarousel({
         items: 4,
         nav: true,
-        navText : ["<img src='img/left-arrow.png'>","<img src='img/right-arrow.png'>"]
+        navText: ["<img src='img/left-arrow.png'>", "<img src='img/right-arrow.png'>"]
         // singleItem: true,
         // dots: true,
         // dotsContainer: '#carousel-custom-dots'
@@ -15,17 +15,16 @@ $(document).ready(function () {
         thumbsPrerendered: true
     });
 
-    /*$("#owl-slider-v2").owlCarousel({
-        items: 1,
-        singleItem: true,
-        dots: true,
-        dotsContainer: '#carousel-custom-dots',
-        animateOut: 'fadeOut',
-        autoHeight:true
-    });
+    function active_burger() {
+        $('.header__burger-wrapper').removeClass('active');
+    }
 
-    $('.owl-dot').click(function () {
-        $("#owl-slider").trigger('to.owl.carousel', [$(this).index(), 300]);
-        $("#owl-slider-v2").trigger('to.owl.carousel', [$(this).index(), 300]);
-    });*/
+    $('#check-menu').on('click',function () {
+        if ($(this).is(':checked')) {
+            $('.header__burger-wrapper').addClass('active');
+            console.log('checkd');
+        } else {
+            setTimeout(active_burger, 500);
+        }
+    });
 });
